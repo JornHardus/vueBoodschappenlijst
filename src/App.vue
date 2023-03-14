2a8a01b6d048cffe24b6a85bf18267a963136d2d
JornHardus <113098382+JornHardus@users.noreply.github.com>
2023-03-14 11:39:31 +0100

    composition api grocery list

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {reactive, ref, computed} from 'vue'
+
+const groceries = reactive([
+  {name:'kaas', price:'5', quantity:'1'},
+  {name:'wol', price:'1', quantity:'1'},
+  {name:'Augurk', price:'3', quantity:'1'},
+
+]);
+
+function submit() {
+  this.$refs.form.$el.submit()
+}
+
+
+const calculateTotal = computed(() => {
+  return groceries.reduce((totalPrice, item) => totalPrice += item.price * item.quantity, 0);
+});
+
+</script>
+
+<template>
+  <p>boodschappen</p>
+  <form ref="form">
+  <li v-for ="groc in groceries">{{ groc }} <input type="number" v-model="groc.quantity"></li>
+  <td>{{calculateTotal}} Euro</td>
+</form>
+</template>
